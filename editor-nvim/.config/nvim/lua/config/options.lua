ceadfa53719396484a268e9a03253620532259fb
christian <christianwilliams1001@tuta.com>
2025-12-20 20:27:10 -0600

    fix: Attempt to fix Neovim treesitter markdown issue
    - Updated LazyVim plugins (lazy-lock.json)
    - Disabled markdown_treesitter in options.lua
    - Issue persists, will debug properly later
    - Workaround: use vim -u NONE for markdown files

--- editor-nvim/.config/nvim/lua/config/options.lua
+++ editor-nvim/.config/nvim/lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = false
+vim.g.markdown_treesitter = false
 
 -- === Custom Productivity Options ===
 local opt = vim.opt
