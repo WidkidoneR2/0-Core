4415d192b8ca3cda826096211c0478703e6fe7fd
christian <christianwilliams1001@tuta.com>
2026-05-30 08:06:08 -0500

    INT-328: Forest R&D Environment -- INT-328 N3: package get-version (first tool derivation)

--- flake.nix
+++ flake.nix
@@ -3,10 +3,30 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations.faelight-vm = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs }:
+    let
       system = "x86_64-linux";
-      modules = [ ./hosts/vm/configuration.nix ];
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      nixosConfigurations.faelight-vm = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [ ./hosts/vm/configuration.nix ];
+      };
+
+      packages.${system}.get-version = pkgs.rustPlatform.buildRustPackage {
+        pname = "get-version";
+        version = "4.0.0";
+        src = ./.;
+
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+          outputHashes = {
+            "smithay-0.7.0" = nixpkgs.lib.fakeHash;
+          };
+        };
+
+        cargoBuildFlags = [ "-p" "get-version" ];
+        doCheck = false;
+      };
     };
-  };
 }
