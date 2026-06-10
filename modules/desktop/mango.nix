fc0afb1f97309579c7aafb99ebdf3822f2832673
WidkidoneR2 <WidkidoneR2@users.noreply.github.com>
2026-06-09 19:06:12 -0500

    Revert "fix: faelight-notify as systemd user service -- stderr to journal, no terminal noise"
    This reverts commit 063353d07213c4ecbe373b0fc939d41824ad5049.

--- modules/desktop/mango.nix
+++ modules/desktop/mango.nix
@@ -17,19 +17,6 @@
       Type=Application
     '';
 
-    # Autostart faelight-notify with stderr suppressed
-    systemd.user.services.faelight-notify = {
-      description = "Faelight notification daemon";
-      wantedBy = [ "graphical-session.target" ];
-      partOf = [ "graphical-session.target" ];
-      serviceConfig = {
-        ExecStart = "${pkgs.faelight-forest}/bin/faelight-notify";
-        Restart = "on-failure";
-        RestartSec = "2s";
-        StandardError = "journal";
-      };
-    };
-
     # GPU / DRM access
     hardware.graphics.enable = true;
     services.udev.packages = [ pkgs.libinput ];
