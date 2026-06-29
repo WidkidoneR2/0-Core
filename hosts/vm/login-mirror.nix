1820373d0527593c6aeb613242eea86ae5afeaa8
WidkidoneR2 <WidkidoneR2@users.noreply.github.com>
2026-06-28 23:59:50 -0500

    framework16: candy tuigreet final (white F-keys, all stages)

--- hosts/vm/login-mirror.nix
+++ hosts/vm/login-mirror.nix
@@ -8,5 +8,5 @@
   time.timeZone = "America/Chicago";  # match host
 
   services.greetd.settings.default_session.command =
-    "${pkgs.tuigreet}/bin/tuigreet --time --remember-session --sessions /etc/greetd/sessions --cmd mango";
+    "${pkgs.tuigreet}/bin/tuigreet --time --remember-session --sessions /etc/greetd/sessions --greeting \"Welcome to Faelight Forest\" --theme 'border=green;title=lightgreen;greet=lightgreen;text=lightcyan;time=lightgreen;prompt=lightcyan;input=lightgreen;action=lightyellow;button=lightmagenta;container=black' --cmd mango";
 }
